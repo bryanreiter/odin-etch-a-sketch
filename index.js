e6cddcbe0128bbf3eab68fa9bfc315be87a0ae3f
bryanreiter <91745125+bryanreiter@users.noreply.github.com>
2023-11-19 23:55:16 -0500

    16x16 grid, doesn't fit in container

---
+++ index.js
@@ -0,0 +1,21 @@
+// canvasContainer is where the grid will be formed
+const canvasContainer = document.getElementById('canvas-container')
+
+function createGrid(rows, cols){
+    //this clears any previous content
+    canvasContainer.innerHTML = ''
+
+
+    for (let i=0;i<rows;i++){
+        for(let j=0;j<cols;j++){
+            const pixel = document.createElement('div')
+            pixel.className = 'pixel'
+            canvasContainer.appendChild(pixel)
+        }
+    }
+
+    canvasContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr`
+}
+
+
+createGrid(16,16);
