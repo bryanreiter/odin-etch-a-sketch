d178a3f388520285b57747b7a1a1bc3666c295f7
Bryan Reiter <tul13138@temple.edu>
2023-11-21 21:08:18 -0500

    Added an event listener for coloring

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-//When page loads, auto select 16x16 size and creategrid based on that.
-document.addEventListener('DOMContentLoaded', function(){
-    //get the pixel count dropdown element
-    const pixelCountDropdown = document.getElementById('pixel-count-dropdown') 
-    //store the initial selected pixel count
-    let selectedPixelCount = pixelCountDropdown.value
-
-    //Event listener when selectedPixelCount changes.
-    pixelCountDropdown.addEventListener('change', function(){
-        selectedPixelCount = pixelCountDropdown.value
-        console.log('Selected Pixel Count:', selectedPixelCount)
-
-        //create new grid
-        createGrid(selectedPixelCount, selectedPixelCount);
-        updatePixelSize(selectedPixelCount);
-    });
-
-    console.log('Initial Pixel Count:', selectedPixelCount);
-    //create initial grid
-    createGrid(selectedPixelCount, selectedPixelCount);
-    updatePixelSize(selectedPixelCount);
-});
-
-
 function createGrid(rows, cols){
 
     // canvasContainer is where the grid will be formed
@@ -54,3 +30,36 @@ function updatePixelSize(pixelCount){
 
 
 }
+
+//When page loads, auto select 16x16 size and creategrid based on that.
+document.addEventListener('DOMContentLoaded', function(){
+    //get the pixel count dropdown element
+    const pixelCountDropdown = document.getElementById('pixel-count-dropdown') 
+    //store the initial selected pixel count
+    let selectedPixelCount = pixelCountDropdown.value
+
+    //Event listener when selectedPixelCount changes.
+    pixelCountDropdown.addEventListener('change', function(){
+        selectedPixelCount = pixelCountDropdown.value
+        console.log('Selected Pixel Count:', selectedPixelCount)
+
+        //create new grid
+        createGrid(selectedPixelCount, selectedPixelCount);
+        updatePixelSize(selectedPixelCount);
+    });
+
+    console.log('Initial Pixel Count:', selectedPixelCount);
+    //create initial grid
+    createGrid(selectedPixelCount, selectedPixelCount);
+    updatePixelSize(selectedPixelCount);
+
+    const canvasContainer = document.getElementById('canvas-container');
+
+    canvasContainer.addEventListener('mouseover', function(event){
+        const target = event.target
+
+        if(target.classList.contains('pixel')){
+            target.classList.add('color-blue')
+        }
+    });
+});
